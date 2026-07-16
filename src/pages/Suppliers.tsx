57aec75d9d87d6183b0368029f606bfb62dc63cc
heamanth-007 <heamanthprabhu59@gmail.com>
2026-07-16 12:00:10 +0530

    feat: add Suppliers page with CRUD UI for vendor management

--- src/pages/Suppliers.tsx
+++ src/pages/Suppliers.tsx
@@ -565,7 +565,7 @@ export const Suppliers: React.FC = () => {
                   <div className="flex flex-col gap-1.5">
                     <label className="text-[12px] font-bold text-[#475569]">Business Contact Email</label>
                     <div className="relative">
-                      <Mail className="absolute left-3.5 top-1/2 -translate-y-1/2 text-[#94a3b8]" size={16} className="pointer-events-none" />
+                      <Mail className="absolute left-3.5 top-1/2 -translate-y-1/2 text-[#94a3b8] pointer-events-none" size={16} />
                       <input
                         type="email"
                         value={editEmail}
